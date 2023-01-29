10f872b713051bf085af7f73f8107311e545a923
Jason <jason@guitarjason.com>
2023-01-28 18:08:33 -0600

    add icons to homepage

--- public/js/home.js
+++ public/js/home.js
@@ -3,39 +3,49 @@ const randomSquares = document.getElementById("randomSquares");
 const handleMouseOver = (eachSquare) => {
   // Generate a random x and y coordinate for the div to move to
   const newX = Math.random() * window.innerWidth;
-  const newY = Math.random() * window.innerHeight;
+  const newY = Math.random() * window.innerHeight - window.innerHeight * 0.3;
 
-  //   const currentX = this.target.style.left;
-  //   const currentY = this.target.style.top;
-
-  eachSquare.target.style.left = newX + "px";
+  eachSquare.target.style.right = newX + "px";
   eachSquare.target.style.top = newY + "px";
 };
-// const handleMouseOut = (eachSquare) => {
-//   // Put the square back to its original position
-//   eachSquare.target.style.left = "0px";
-//   eachSquare.target.style.top = "0px";
-// };
 
 const createSquares = (x, y) => {
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < 40; i++) {
     let squares = document.createElement("div");
     squares.className = "allSquares";
     x.appendChild(squares);
     squares.id = y + i;
 
     //   Math.random() * (max - min) + min;
-    squares.style.width = Math.floor(Math.random() * (150 - 50) + 50) + "px";
-    squares.style.height = Math.floor(Math.random() * (150 - 50) + 50) + "px";
+    squares.style.width = Math.floor(Math.random() * (150 - 60) + 60) + "px";
+    squares.style.height = Math.floor(Math.random() * (150 - 60) + 60) + "px";
     squares.style.right = Math.floor(Math.random() * 100) + "vw";
-    squares.style.top = Math.floor(Math.random() * 100 + 7) + "vh";
+    squares.style.top = Math.floor(Math.random() * 75) + "vh";
     squares.style.backgroundColor =
-      "var(--bg-" + Math.floor(Math.random() * 5) + ")";
+      "var(--bg-" + Math.floor(Math.random() * 6) + ")";
 
     const eachSquare = document.getElementById(`color${i}`);
     eachSquare.addEventListener("mouseover", handleMouseOver);
-    // eachSquare.addEventListener("mouseout", handleMouseOut);
+    // eachSquare.addEventListener("mouseover", handleMouseOut);
   }
 };
 
 createSquares(randomSquares, "color");
+
+//making things animate on scroll in
+//
+//
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
