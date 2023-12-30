f71ffc8c5d3f900ec94b0068149faa4f43cbcc33
Jason <jason@guitarjason.com>
2023-12-30 08:07:44 -0700

    fix text speed

--- public/js/survival.js
+++ public/js/survival.js
@@ -77,7 +77,7 @@ function slowInText(text) {
     hideButton();
     if (narrativeTextArray.length > 0) {
       narrativeText.innerHTML += narrativeTextArray.shift();
-      loopTimer = setTimeout(recursiveSlowInText, 50);
+      loopTimer = setTimeout(recursiveSlowInText, 40);
     } else {
       canClick = true;
       showButton();
@@ -156,7 +156,7 @@ function changeUserDisplayText(option) {
   switch (option) {
     case "pressEject":
       startSlowInText(
-        "You are jettisoned out of the pod and land 100 meters away. You lay there stunned. You hear a rustling noise."
+        "You are jettisoned out of the pod and land 100 meters away.            You lay there stunned.              You hear a rustling noise."
       );
       setTimeout(function () {
         changeButtontext("Lie still and wait", "Grab the closest rock");
@@ -167,14 +167,14 @@ function changeUserDisplayText(option) {
 
     case "grabRock":
       startSlowInText(
-        "You flail your arms at your sides in hopes to grab a rock to defend yourself. Before you find one, you hear a whoosh and an immediate hissing noise. Your suit is pierced by a spear. You suffocate within 30 seconds. You are dead."
+        "You flail your arms at your sides in hopes to grab a rock to defend yourself.         Before you find one, you hear a whoosh and an immediate hissing noise.           Your suit is pierced by a spear.          You suffocate within 30 seconds.          You are dead."
       );
       removeButton();
       break;
 
     case "lieStill":
       startSlowInText(
-        "You stay completely still. Out of the corner of your eye, you see a figure emerge from around a rock."
+        "You stay completely still.          Out of the corner of your eye,       you see a figure emerge from around a rock."
       );
       setTimeout(function () {
         changeButtontext("Wave at the figure", "Say 'hello' to the figure");
@@ -185,7 +185,7 @@ function changeUserDisplayText(option) {
 
     case "hello":
       startSlowInText(
-        "You speak the word 'hello' to the figure. After remaining motionless for a moment, the figure then walks towards you. You notice the creature is armed with a spear. It continues walking towards you, while holding the spear out."
+        "You speak the word 'hello' to the figure.          After remaining motionless for a moment,      the figure then walks towards you.        You notice the creature is armed with a spear.        It continues walking towards you,      while holding the spear out."
       );
       setTimeout(function () {
         changeButtontext("Say 'I come in peace'", "Grab the alien's weapon");
@@ -196,14 +196,14 @@ function changeUserDisplayText(option) {
 
     case "grabWeapon":
       startSlowInText(
-        "You attempt to grab the creature's spear. Your speed is no match for this being. Before you come close to grabbing the weapon, it is thrust into your neck. A hissing noise and a warm feeling rush down your chest. You both suffocate and bleed out. You are dead."
+        "You attempt to grab the creature's spear.        Your speed is no match for this being.        Before you come close to grabbing the weapon,      it is thrust into your neck.      A hissing noise and a warm feeling rush down your chest.       You both suffocate and bleed out.       You are dead."
       );
       removeButton();
       break;
 
     case "peace":
       startSlowInText(
-        "The figure lowers its spear. It looks at you for some time and finally emits a noise. It sounds like a high-pitched ringing. You can't understand it, but it doesn't sound hostile."
+        "The figure lowers its spear.       It looks at you for some time and finally emits a noise.        It sounds like a high-pitched ringing.       You can't understand it,        but it doesn't sound hostile."
       );
       setTimeout(function () {
         changeButtontext(
@@ -217,14 +217,14 @@ function changeUserDisplayText(option) {
 
     case "pointCrash":
       startSlowInText(
-        "You motion towards your crash site, hoping the creature can help you understand what happened. The creature looks at your wrecked ship, pauses, and then turns you around and motions towards a valley. Scanning the valley, you notice several shiny objects. Looking closer, the objects are other ships that have crashed here. You realize you aren't the first person to crash. Your hope fades, you slump down, there is no making it off the planet, you are going to die here..."
+        "You motion towards your crash site,       hoping the creature can help you understand what happened.       The creature looks at your wrecked ship,      pauses,       and then turns you around and motions towards a valley.         Scanning the valley,        you notice several shiny objects.      Looking closer,       the objects are other ships that have crashed here.       You realize you aren't the first person to crash.        Your hope fades,      you slump down,        there is no making it off the planet,       you are going to die here..."
       );
       removeButton();
       break;
 
     case "pointCreature":
       startSlowInText(
-        "You point at the creature, hoping for some sort of explanation of what it is doing here. The creature seems to understand. It motions for you to follow."
+        "You point at the creature,       hoping for some sort of explanation of what it is doing here.        The creature seems to understand.         It motions for you to follow."
       );
       setTimeout(function () {
         changeButtontext("Follow the creature", "Go back to your ship");
@@ -235,14 +235,14 @@ function changeUserDisplayText(option) {
 
     case "backToShip":
       startSlowInText(
-        "You head back to your ship in hopes to leave this place. You start the walk back. You hear a growling noise. You notice there are several animals circling your newly crashed ship. They spot you. You attempt to run. They are much faster. You are dead."
+        "You head back to your ship in hopes to leave this place.       You start the walk back.       You hear a growling noise.         You notice there are several animals circling your newly crashed ship.       They spot you.        You attempt to run.       They are much faster.         You are dead."
       );
       removeButton();
       break;
 
     case "followCreature":
       startSlowInText(
-        "You follow the creature. It leads you through a small valley and over a mountain. You reach your destination at what appears to be the creature's ship. It leads you through to the back of the ship. It motions at what you assume to be the engines. They look intact, but don't appear to have any fuel. You notice it is getting dark."
+        "You follow the creature.         It leads you through a small valley and over a mountain.       You reach your destination at what appears to be the creature's ship.        It leads you through to the back of the ship.        It motions at what you assume to be the engines.        They look intact,      but don't appear to have any fuel.       You notice it is getting dark."
       );
       setTimeout(function () {
         changeButtontext(
@@ -256,14 +256,14 @@ function changeUserDisplayText(option) {
 
     case "fuelNow":
       startSlowInText(
-        "You are in a hurry to get off the planet. You decide to go get your ship's remaining fuel. You and the creature set out at night. You make it halfway. The light from this system's star crosses the horizon. The temperature drops drastically. You and the creature freeze within minutes. You are dead."
+        "You are in a hurry to get off the planet.        You decide to go get your ship's remaining fuel.       You and the creature set out at night.      You make it halfway.       The light from this system's star crosses the horizon.        The temperature drops drastically.          You and the creature freeze within minutes.          You are dead."
       );
       removeButton();
       break;
 
     case "fuelTomorrow":
       startSlowInText(
-        "You decide it is safer to venture in daylight. You and the creature set up camp inside its ship for the night. Sleep does not come easily. You wake at first light. You are exhausted. The creature is nowhere to be seen."
+        "You decide it is safer to venture in daylight.        You and the creature set up camp inside its ship for the night.        Sleep does not come easily.         You wake at first light.       You are exhausted.        The creature is nowhere to be seen."
       );
       setTimeout(function () {
         changeButtontext("Set out alone", "Search for the creature");
@@ -274,14 +274,14 @@ function changeUserDisplayText(option) {
 
     case "setOutAlone":
       startSlowInText(
-        "You set out to get the fuel alone. You make it over the mountain. The view from the top does not look familiar. You choose a direction and start walking. You don't recognize anything. You are lost. You never find your ship or the creature again. You perish after three days of searching."
+        "You set out to get the fuel alone.       You make it over the mountain.         The view from the top does not look familiar.         You choose a direction and start walking.       You don't recognize anything.        You are lost.       You never find your ship or the creature again.        You perish after three days of searching."
       );
       removeButton();
       break;
 
     case "searchCreature":
       startSlowInText(
-        "You search for the creature. You call out for it, with no response. You decide to search the ship. There are numerous rooms to search."
+        "You search for the creature.       You call out for it,       with no response.       You decide to search the ship.         There are numerous rooms to search."
       );
       setTimeout(function () {
         changeButtontext("Search the red room", "Search the blue room");
@@ -292,28 +292,28 @@ function changeUserDisplayText(option) {
 
     case "redRoom":
       startSlowInText(
-        "You venture through a red door. You walk into the room, an alarm goes off. A beam of light comes from the center of the room. The light gets brighter and hotter. You don't have enough time to realize what is happening. You are vaporized before you can turn around. You are dead."
+        "You venture through a red door.         You walk into the room,       an alarm goes off.        A beam of light comes from the center of the room.        The light gets brighter and hotter.        You don't have enough time to realize what is happening.         You are vaporized before you can turn around.        You are dead."
       );
       removeButton();
       break;
 
     case "blueRoom":
       startSlowInText(
-        "You venture through a blue door. The room is large. You walk further in. An alarm goes off. The door slams behind you. A hissing noise starts. It gets harder to breathe. You pry at the door with no result. You suffocate. You are dead."
+        "You venture through a blue door.        The room is large.        You walk further in.        An alarm goes off.        The door slams behind you.        A hissing noise starts.        It gets harder to breathe.        You pry at the door with no result.         You suffocate.       You are dead."
       );
       removeButton();
       break;
 
     case "wave":
       startSlowInText(
-        "The figure mistakes your motion as hostile. It lets out a loud shriek. From behind you, a rock comes flying and crushes your helmet. You are dead."
+        "The figure mistakes your motion as hostile.        It lets out a loud shriek.        From behind you,      a rock comes flying and crushes your helmet.       You are dead."
       );
       removeButton();
       break;
 
     case "checkSurroundings":
       startSlowInText(
-        "You see a pile of wreckage around you. You spot what appear to be two different containers, one small and one large."
+        "You see a pile of wreckage around you.       You spot what appear to be two different containers,       one small and one large."
       );
       changeButtontext(
         "Go open the small container",
@@ -325,7 +325,7 @@ function changeUserDisplayText(option) {
 
     case "smallContainer":
       startSlowInText(
-        "You walk towards the small container. You open it to find a weapon. A gun of some sort."
+        "You walk towards the small container.        You open it to find a weapon.      A gun of some sort."
       );
       setTimeout(function () {
         changeButtontext("Take the weapon", "Leave the weapon");
@@ -336,21 +336,21 @@ function changeUserDisplayText(option) {
 
     case "leaveWeapon":
       startSlowInText(
-        "You leave the weapon behind. You walk back towards your ship. You hear a growling behind the large container. You scramble to re-enter your ship for safety. You don't make it. You are dead."
+        "You leave the weapon behind.        You walk back towards your ship.        You hear a growling behind the large container.         You scramble to re-enter your ship for safety.        You don't make it.       You are dead."
       );
       removeButton();
       break;
 
     case "takeWeapon":
       startSlowInText(
-        "You grab the weapon. It starts hissing. You experience heat emanating from the handle. You decide to throw the weapon away from you for safety. As you swing your arm back, the weapon discharges. A large blast occurs. You are dead."
+        "You grab the weapon.        It starts hissing.        You experience heat emanating from the handle.         You decide to throw the weapon away from you for safety.        As you swing your arm back,        the weapon discharges.      A large blast occurs.      You are dead."
       );
       removeButton();
       break;
 
     case "largeContainer":
       startSlowInText(
-        "You make your way to the large container. You open the doors. Your eyes focus on a black shadow in the back of the container. The shadow growls. You don't have time to react. The shadow moves and surrounds you. Everything goes black. You are dead."
+        "You make your way to the large container.        You open the doors.         Your eyes focus on a black shadow in the back of the container.        The shadow growls.         You don't have time to react.         The shadow moves and surrounds you.         Everything goes black.       You are dead."
       );
       removeButton();
       break;
